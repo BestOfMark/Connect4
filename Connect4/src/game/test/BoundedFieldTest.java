5bb954f04c7375fb3d1ec5a3efbf17472809ad5a
Mark <Mark@Mark-PC>
2017-02-01 21:14:23 +0100

    Updated BoundedFieldTest.java

--- Connect4/src/game/test/BoundedFieldTest.java
+++ Connect4/src/game/test/BoundedFieldTest.java
@@ -11,14 +11,16 @@ public class BoundedFieldTest {
 
 	private static final int WIN = 4;
 	private static final int BF1_X = 4, BF1_Y = 4, BF1_Z = 4;
-	private static final int BF2_X = 4, BF2_Y = 4, BF2_Z = 4;
-	private BoundedField bf1, bf2;
+	private static final int BF2_X = 4, BF2_Y = 3, BF2_Z = 3;
+	private static final int BF3_X = 1, BF3_Y = 1, BF3_Z = 2;
+	private BoundedField bf1, bf2, bf3;
 	private Chip c1, c2;
 	
 	@Before
 	public void setUp() throws Exception {
 		bf1 = new BoundedField(BF1_X, BF1_Y, BF1_Z, WIN);
 		bf2 = new BoundedField(BF2_X, BF2_Y, BF2_Z, WIN);
+		bf3 = new BoundedField(BF3_X, BF3_Y, BF3_Z, WIN);
 		c1 = Chip.RED;
 		c2 = Chip.YELLOW;
 		for (int z = 0; z < BF2_Z; z++) {
@@ -91,6 +93,26 @@ public class BoundedFieldTest {
 	public void printString() {
 		System.out.println(bf1.toString());
 		System.out.println(bf2.toString());
+	}
+	
+	@Test
+	public void checkFull() {
+		Assert.assertFalse(bf1.checkFull());
+		Assert.assertFalse(bf2.checkFull());
+		Assert.assertFalse(bf3.checkFull());
+		bf3.addChip(c1, 0, 0);
+		Assert.assertFalse(bf3.checkFull());
+		bf3.addChip(c2, 0, 0);
+		Assert.assertTrue(bf3.checkFull());
+	}
+	
+	@Test
+	public void getColumn() {
+		Chip[] expectedCol = new Chip[BF2_Z];
+		for (int i = 0; i < BF2_Z - 1; i++) {
+			expectedCol[i] = c2;
+		}
+		Assert.assertArrayEquals(expectedCol, bf2.getColumn(BF2_X - 1, BF2_Y - 1));
 	}
 
 }
